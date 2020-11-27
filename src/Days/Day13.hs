b7b634e47d8de8bbd4d439373607f4915e2bcc77
Sam Coy <samcoy3@gmail.com>
2020-11-27 17:57:51 +0000

    Made the import of Data.Map the Strict variety

--- src/Days/Day13.hs
+++ src/Days/Day13.hs
@@ -2,8 +2,8 @@ module Days.Day13 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
