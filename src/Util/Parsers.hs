9ffc5e198ecf3e2eecf7316f2af34c59be62e68b
Sam Coy <samcoy3@gmail.com>
2020-12-02 15:19:42 +0000

    Added around parser combinator to Util

--- src/Util/Parsers.hs
+++ src/Util/Parsers.hs
@@ -1,12 +1,14 @@
-module Util.Parsers (coordinateParser) where
+module Util.Parsers where
 
 import Data.Attoparsec.Text
 import Data.Map (Map)
 import qualified Data.Map as Map
 
 {-
-This module contains a list of parsers which are likely to be useful for Advent of Code problems.
+This module contains a list of parsers and combinators which are likely to be useful for Advent of Code problems.
 -}
+
+------------ PARSERS ------------
 
 -- Takes a "mapper" "function that might map a char to a datatype, and an initial index (usually 0 or 1)
 -- Returns a parser that returns a map from coordinates to all instances where the function returns Just
@@ -23,3 +25,13 @@ coordinateParser mapper start = coordinateParser' start start
           return Map.empty
         ]
     addToMap mapper x y c = Map.alter (const (mapper c)) (x, y)
+
+------------ COMBINATORS ------------
+
+-- Takes a parser and a separator. Parses one instance of the parser before the separator and one afterwards, returning the parsed values as a pair.
+around :: Parser a -> Parser b -> Parser (a, a)
+around p sep = do
+  a <- p
+  sep
+  b <- p
+  return (a, b)
